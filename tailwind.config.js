cd8c2f527643ea7c8444d1243725f92d5f8cbc79
Teninlanimi Taiwo <taiwoteninlanimi@gmail.com>
2024-11-25 18:57:28 +0100

    Fixed Transparency Issue Finally

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,7 +11,7 @@ export default {
         purple:'#560677',
         blue:'#628EFF',
         black: '#000000',
-        background:'rgb(30,30,30)',
+        background:'rgb(31,31,28)',
         secondary:'#9ca3af'
       },
       borderRadius:{
